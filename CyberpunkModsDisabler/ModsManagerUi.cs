cb9859ecf1a4b73a4062d402d65674a0e23bea58
bkisly <b.kisly@gmail.com>
2022-08-06 01:40:22 +0200

    Refactored the code

--- CyberpunkModsDisabler/ModsManagerUi.cs
+++ CyberpunkModsDisabler/ModsManagerUi.cs
@@ -1,72 +1,65 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace CyberpunkModsDisabler;
 
-namespace CyberpunkModsDisabler
+internal class ModsManagerUi
 {
-    internal class ModsManagerUi
+    private readonly ModsManager _modsManager;
+
+    public ModsManagerUi() => _modsManager = new();
+
+    public void Init()
     {
-        private readonly ModsManager _modsManager;
+        Console.WriteLine("----------\nCYBERPUNK 2077 MODS DISABLER\n----------");
+        Console.WriteLine($"Current mods status: {_modsManager.ModsStatus}");
 
-        public ModsManagerUi() => _modsManager = new();
-
-        public void Init()
+        switch(_modsManager.ModsStatus)
         {
-            Console.WriteLine("----------\nCYBERPUNK 2077 MODS DISABLER\n----------");
-            Console.WriteLine($"Current mods status: {_modsManager.ModsStatus}");
-
-            switch(_modsManager.ModsStatus)
-            {
-                case ModsStatus.NoMods:
-                    CloseMessage();
-                    return;
-                case ModsStatus.Corrupted:
-                    MoveModsToSelectedDest();
-                    return;
-            }
-
-            Console.WriteLine("Press q to close the program or any other key to move the mods: ");
-
-            ConsoleKeyInfo keyInfo = Console.ReadKey(true);
-            if(keyInfo.KeyChar != 'q')
-            {
-                if (_modsManager.ModsStatus == ModsStatus.Enabled) _modsManager.MoveToHidden();
-                else if (_modsManager.ModsStatus == ModsStatus.Disabled) _modsManager.MoveToOriginal();
-
-                Console.WriteLine("The operation has been performed successfully.");
+            case ModsStatus.NoMods:
                 CloseMessage();
-            }
+                return;
+            case ModsStatus.Corrupted:
+                MoveModsToSelectedDest();
+                return;
         }
 
-        private void MoveModsToSelectedDest()
+        Console.WriteLine("Press q to close the program or any other key to move the mods: ");
+
+        ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+        if(keyInfo.KeyChar != 'q')
         {
-            Console.WriteLine("This situation happens when there are mods either in the original location and in the hidden folder.");
-            Console.WriteLine("Select the action to perform:");
-            Console.WriteLine("1 - move mods from the original location to the hidden folder");
-            Console.WriteLine("2 - move mods from the hidden folder to the original location");
-            Console.WriteLine("any other key - close the program");
-
-            ConsoleKeyInfo keyInfo = Console.ReadKey(true);
-
-            switch (keyInfo.KeyChar)
-            {
-                case '1':
-                    _modsManager.MoveToHidden();
-                    break;
-                case '2':
-                    _modsManager.MoveToOriginal();
-                    break;
-            }
+            if (_modsManager.ModsStatus == ModsStatus.Enabled) _modsManager.MoveMods();
+            else if (_modsManager.ModsStatus == ModsStatus.Disabled) _modsManager.MoveMods(false);
 
             Console.WriteLine("The operation has been performed successfully.");
+            CloseMessage();
+        }
+    }
+
+    private void MoveModsToSelectedDest()
+    {
+        Console.WriteLine("This situation happens when there are mods either in the original location and in the hidden folder.");
+        Console.WriteLine("Select the action to perform:");
+        Console.WriteLine("1 - move mods from the original location to the hidden folder");
+        Console.WriteLine("2 - move mods from the hidden folder to the original location");
+        Console.WriteLine("any other key - close the program");
+
+        ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+
+        switch (keyInfo.KeyChar)
+        {
+            case '1':
+                _modsManager.MoveMods();
+                break;
+            case '2':
+                _modsManager.MoveMods(false);
+                break;
         }
 
-        private static void CloseMessage()
-        {
-            Console.WriteLine("Press any key to close the program...");
-            Console.ReadKey(true);
-        }
+        Console.WriteLine("The operation has been performed successfully.");
+    }
+
+    private static void CloseMessage()
+    {
+        Console.WriteLine("Press any key to close the program...");
+        Console.ReadKey(true);
     }
 }
